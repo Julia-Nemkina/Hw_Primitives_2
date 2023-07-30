802a2dfebb2f47744afc0415f03d76b70f829eb3
Julia Nemkina <nemkina.julia@mail.ru>
2023-07-30 20:18:10 +0300

    Changes

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        int start = 150;
+        int add = 1350;
+        int sum = start + add;
+        int bonus;
+
+        if (add >= 1000) {
+            bonus = add / 100;
+        } else {
+            bonus = 0;
+        }
+        int balance = sum + bonus;
+
+        System.out.println("Итоговый счёт:" + balance);
+    }
 }
